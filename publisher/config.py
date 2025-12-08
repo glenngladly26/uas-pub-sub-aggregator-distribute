9c52c02adaec66f9fe5c3fa5721fe18520796107
Glenn Gladly <glenngladlygessong@gmail.com>
2025-12-08 18:34:53 +0800

    chore: change total events from 1000 to 20000

--- publisher/config.py
+++ publisher/config.py
@@ -2,7 +2,7 @@ import os
 
 class Settings:
     TARGET_URL: str = os.getenv("TARGET_URL", "http://aggregator:8080/publish")
-    TOTAL_EVENTS: int = os.getenv("TOTAL_EVENTS", 1000)
+    TOTAL_EVENTS: int = os.getenv("TOTAL_EVENTS", 20000)
     DUPLICATE_RATIO: float = 0.3
     CONCURRENCY: int = 50
     TOPICS: list = ["payment", "order", "user-login", "sensor-suhu", "notifikasi"]
